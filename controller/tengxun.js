6a1e24e71ce7997f38937f6366c0ca15935ba2f6
孔磊 <18296155494@163.com>
2016-10-26 09:19:26 +0800

    k

--- controller/tengxun.js
+++ controller/tengxun.js
@@ -2,6 +2,7 @@ var News = require('../model/newsModel');
 
 exports.tengxunList = function (req, res) {
 	News.find((err, news) => {
+    console.log(news)
 		res.render('tengxun',{news:news});
 	})
 }
